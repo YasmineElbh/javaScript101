fcce80cf82c8485770a63bdb5dd8f8981bbea44f
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-07-20 22:54:31 +0100

    operator

---
+++ operator/main.js
@@ -0,0 +1,16 @@
+/**
+ * tasks:
+ * create a num1 variable with the value 10, and a num2 with the varibale 20
+ * console log num1+num2
+ * find the module of the two variables
+ * create a vaiable called total and set rthe value 20, add one to the value and console log it
+ */
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 + num2);
+console.log(num1 % num2);
+
+let total = 10;
+total++;
+console.log(total);
