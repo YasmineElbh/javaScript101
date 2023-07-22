898e4beaf2016697c2a9c61134c33405ac1c7d93
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-07-22 17:13:35 +0100

    numbers

---
+++ DataType/numbers/main.js
@@ -0,0 +1,17 @@
+const PI = 3.141596254;
+console.log(PI.toFixed(3));//3.142
+
+let mil = 10000000;
+console.log(mil.toExponential());//1e+7
+
+let hex = 0xff;
+console.log(hex);//255
+
+//toString
+let h = 255;
+console.log(h.toString(16));//ff
+console.log(h.toString(2));//11111111
+console.log(h.toString(10));//255
+
+
+
