15db5c19ff22063b6ed6da5d72058ed30ac09a8f
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-09-17 20:26:20 +0100

    exercice

--- oop/les classes/ex1.js
+++ oop/les classes/ex1.js
@@ -13,5 +13,27 @@ class Car{
         this.speed = speed
         this.model = model
     }
+    getInfo()
+    {
+        if(this.name === undefined || this.color === undefined ||
+            this.price === undefined || this.speed === undefined || this.model === undefined)
+            {
+                return 'Please Fill Data ...'
+            }
+            else{
+        return `${this.name} Car\nName: ${this.name}
+        \nPrice: ${this.price}$\nColor: ${this.color}
+        \nSpeed: ${this.speed}Km/h\nModel:${this.model}`
+    }
 }
-const car1 = new car('dkdk', 'dkdkd', 2234, 2000, 'fjfj')
+//get age
+    getAge(){
+        const CurrYear = new Date().getFullYear()
+        const carAge = CurrYear - this.model
+        return `Car Age is ${carAge} years`
+    }
+}
+const car1 = new Car( 'siat','gkkgkg', 2234, 2000, 2000)
+const car2 = new Car( 'ford', 'black', 30049, 2000, 2020)
+console.log(car1.getInfo())
+console.log(car2.getAge())
